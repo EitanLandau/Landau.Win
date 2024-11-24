9678ab3939ff2918d75034e67f911cebdd0bcec2
eitanlandau2@gmail.com <eitanlandau2@gmail.com>
2024-11-24 17:13:36 +0200

    טבלאות אחת ושתיים

--- Landau.Win/Form1.Designer.cs
+++ Landau.Win/Form1.Designer.cs
@@ -33,9 +33,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(95, 172);
+            this.button1.Location = new System.Drawing.Point(293, 137);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(198, 118);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
