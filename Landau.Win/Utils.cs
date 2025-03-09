4363bbb1f263184c2c6d935f2c06fcc44e127487
eitanlandau2@gmail.com <eitanlandau2@gmail.com>
2025-03-09 16:57:56 +0200

    ואלידאציות אנא עארף

--- Landau.Win/Utils.cs
+++ Landau.Win/Utils.cs
@@ -13,6 +13,11 @@ namespace Landau.Win
     {
         public static bool isValidName(string name, ErrorProvider ep, TextBox txb, string error)
         {
+            if (name == null)
+            {
+                ep.SetError(txb, error);
+                return false;
+            }
             bool a1 = name.Trim().Length > 1;
             bool a2 = !name.Any(Char.IsDigit);
             if (a1 && a2)
@@ -27,11 +32,10 @@ namespace Landau.Win
 
             }
         }
-    public static bool isValidPhoneNumber(string phone, ErrorProvider ep, MaskedTextBox mtxb, string error)
+        public static bool isValidPhoneNumber(string phone, ErrorProvider ep, MaskedTextBox mtxb, string error)
         {
-           bool a1 = true;
-            //bool a1 = phone.Any(Char.IsDigit);
-            bool a2 = phone.Length <= 10;
+            bool a1 = (phone.Trim().Length == 10);
+            bool a2 = phone.Any(Char.IsDigit);
             if (a1 && a2)
             {
                 ep.SetError(mtxb, "");
@@ -40,6 +44,20 @@ namespace Landau.Win
             else
             {
                 ep.SetError(mtxb, error);
+                return false;
+            }
+        }
+        public static bool isValidEmail(string email, ErrorProvider ep, TextBox txb, string error)
+        {
+            bool a1 = email.Trim().Length > 10;
+            if (a1)
+            {
+                ep.SetError(txb, "");
+                return true;
+            }
+            else
+            {
+                ep.SetError(txb, error);
                 return false;
             }
         }
---
+++ Landau.Win/forms/updateCustomerForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Landau.Win.forms
+{
+    public partial class updateCustomerForm : Form
+    {
+        public updateCustomerForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
